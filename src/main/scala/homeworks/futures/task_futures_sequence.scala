fdb5e138cd8b9b7a6ce81235b9c6c53e1569ad0c
Anton Illarionov <illarionovanton@gmail.com>
2020-12-20 20:51:05 +0300

    Solve homework with futures

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -3,6 +3,9 @@ package homeworks.futures
 import homeworks.HomeworksUtils.TaskSyntax
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
 
 object task_futures_sequence {
 
@@ -18,6 +21,19 @@ object task_futures_sequence {
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
-  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `encrypt`"()
+  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] = Future {
+    var successes: List[A] = Nil
+    var failures: List[Throwable] = Nil
+
+    for ( f <- futures.reverse ) {
+      try {
+        successes = Await.result(f, Duration.Inf) :: successes
+      }
+      catch {
+        case exception: Throwable => failures = exception :: failures
+      }
+    }
+
+    (successes, failures)
+  }
 }
